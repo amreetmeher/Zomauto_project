1d22c3eb7b6e54823045bfa7fc5701e5d07e4bc2
Amreet Meher <meheramreet@gmail.com>
2023-07-31 18:55:33 +0530

    first commit

---
+++ src/components/common/footer/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../footer/footer.css";
+
+const Footer = () => {
+  return <div className="absolute-center">Made By Amreet Meher</div>;
+};
+
+export default Footer;
